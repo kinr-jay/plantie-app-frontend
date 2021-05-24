2638c981e39e82e2a9572da91031656e24b2c757
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-24 12:29:01 -0400

    updated the individual plant functions

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -1,7 +1,21 @@
-const Plant = () => {
-    return (
-        <h1>This is the Plant Component!</h1>
-    )
+import React from "react";
+const Plant = (props) => {
+
+const { plants } = props
+// return the jsx for when we have the plants
+const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {plants.map((plant) => (
+        <article key={plant._id}>
+          <img src={plant.img}/>
+          <h1>{plant.name}</h1>
+          <h3>{plant.description}</h3>
+          <h3>{plant.frequency}</h3>
+          <h3>{plant.lastWatered}</h3>
+          <h3>{plant.description}</h3>
+        </article>
+      ))}
+    </div>
 }
 
 export default Plant
