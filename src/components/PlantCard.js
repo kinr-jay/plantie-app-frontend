2b457ca278d9d9ad7add614a824545818701478e
William Truscott <williamtruscott@Williams-MacBook-Pro.local>
2021-05-24 14:35:47 -0400

    working on MyGarden page, added a syle folder to house scss stle sheets

---
+++ src/components/PlantCard.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div className="plantcard">
+            <img src="https://images.squarespace-cdn.com/content/v1/55d143f0e4b0d49a516f32a9/1596319940898-D4HMT75KOXZ0F0U0PD3Y/ke17ZwdGBToddI8pDm48kL7ZiGTVZ4_WkolJkgbeTjl7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Uf0tdRk0l1Q3HtMgVa1wUGSf4x025uUlg8CddhKF8AejVzIgDbKuWbU6v7VINLj_HA/bunny+ear+cactus.jpg?format=1000w" alt="Cactus"/>
+            <h2>Javier</h2>
+            <h4>Bunny-Ear Cactus</h4>
+        </div>
+    )
+}
+
+export default Card
