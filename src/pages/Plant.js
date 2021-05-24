c1df592826d5036185f8506ab0e3da6888baef09
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-24 16:17:30 -0400

    props updated in Plant.js

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -3,19 +3,19 @@ const Plant = (props) => {
 
 const { plants } = props
 // return the jsx for when we have the plants
-const loaded = () => (
-    <div style={{textAlign: "center"}}>
-      {plants.map((plant) => (
+
+     return <div className='individualPlant'>
+    
         <article key={plant._id}>
           <img src={plant.img}/>
           <h1>{plant.name}</h1>
-          <h3>{plant.description}</h3>
-          <h3>{plant.frequency}</h3>
-          <h3>{plant.lastWatered}</h3>
-          <h3>{plant.description}</h3>
+          <h4>Fruequency: {plant.frequency}</h4>
+          <h4>Sunlight: {plant.sunligh}</h4>
+          <h4>Temprature: {plant.tempreture}</h4>
+          <h4>Description: {plant.description}</h4>
         </article>
-      ))}
     </div>
 }
+
 
 export default Plant
