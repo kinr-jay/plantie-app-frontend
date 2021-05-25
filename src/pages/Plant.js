60443d32fc770fa44d8edd51b6cc8bf3cb96d13a
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-25 09:45:35 -0400

    css file added

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -1,27 +1,20 @@
+
 import React from "react";
 const Plant = (props) => {
 
-const { plants } = props
-// return the jsx for when we have the plants
-const loaded = () => (
-    <div className='individualPlant'>
-      {plants.map((plant) => (
+
+     return <div className='individualPlant'>
+    
         <article key={plant._id}>
           <img src={plant.img}/>
           <h1>{plant.name}</h1>
-          <h4>{plant.frequency}</h4>
-          <h4>{plant.sunligh}</h4>
-          <h4>{plant.tempreture}</h4>
-          <h4>{plant.description}</h4>
+          <h4>Fruequency: {plant.frequency}</h4>
+          <h4>Sunlight: {plant.sunligh}</h4>
+          <h4>Temprature: {plant.tempreture}</h4>
+          <h4>Description: {plant.description}</h4>
         </article>
-      ))}
     </div>
-)
-
-// adding the loading function for when there is nothing to display
-const loading = () => <h1>Loading</h1>
-
-return plants.length > 0 ? loaded() : loading()
 }
+
 
 export default Plant
