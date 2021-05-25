7564a508d90a27bcdacb32725a6be3aa18177b0d
kinr-jay <cjacob22@gmail.com>
2021-05-25 12:58:25 -0600

    created and styled Login page. buttons have no functionality yet

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,20 @@
 const Login = () => {
+
     return (
-        <h1>This is the Login Component!</h1>
+      <div className="login-page">
+        <img
+          className="logo"
+          src="https://res.cloudinary.com/kinr-jay/image/upload/v1621966095/GA/plantie%20app/plantie-icon-vector_bjdb6k.svg"
+          alt="plantie logo"
+        />
+        <h1>plantie</h1>
+        <p>Welcome! We will help you to take care of your plants!</p>
+        <button className="sign-up">Sign up</button>
+        <button className="log-in">Log in</button>
+        {/* <p>or use</p>
+        <button className="third-party-auth"></button>
+        <button className="third-party-auth"></button> */}
+      </div>
     )
 }
 
