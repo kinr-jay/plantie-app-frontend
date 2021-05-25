782690e8bd723f4501b0dc0b6cf88620bbc1b14d
PSP92 <78216088+PSP92@users.noreply.github.com>
2021-05-25 08:35:23 -0700

    Update Plant.js

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -1,21 +1,21 @@
-// import React from "react";
-// const Plant = (props) => {
+import React from "react";
+const Plant = (props) => {
 
-// const { plants } = props
-// // return the jsx for when we have the plants
+const { plants } = props
+// return the jsx for when we have the plants
 
-//      return <div className='individualPlant'>
+     return <div className='individualPlant'>
     
-//         <article key={plant._id}>
-//           <img src={plant.img}/>
-//           <h1>{plant.name}</h1>
-//           <h4>Fruequency: {plant.frequency}</h4>
-//           <h4>Sunlight: {plant.sunligh}</h4>
-//           <h4>Temprature: {plant.tempreture}</h4>
-//           <h4>Description: {plant.description}</h4>
-//         </article>
-//     </div>
-// }
+        <article key={plant._id}>
+          <img src={plant.img}/>
+          <h1>{plant.name}</h1>
+          <h4>Fruequency: {plant.frequency}</h4>
+          <h4>Sunlight: {plant.sunligh}</h4>
+          <h4>Temprature: {plant.tempreture}</h4>
+          <h4>Description: {plant.description}</h4>
+        </article>
+    </div>
+}
 
 
-// export default Plant
+export default Plant
