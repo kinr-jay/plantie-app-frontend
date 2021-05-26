10dd6295b6319cb7440a6c8176a06a9f6836bd6e
kinr-jay <cjacob22@gmail.com>
2021-05-25 20:20:30 -0600

    added prop defaults

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -2,7 +2,9 @@ import React from "react"
 import { Link } from "react-router-dom"
 import ProfileDropdown from "../components/ProfileDropdown"
 
-const Profile = ({ garden }) => {
+const Profile = ({
+  garden = { firstName: "AM", lastName: "Squad", email: "test@plantie.com" },
+}) => {
   return (
     <div className="profile-page">
       <h2>Hello {garden.firstName}!</h2>
