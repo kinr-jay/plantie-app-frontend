69c8694c8e1bc4a04a09093add0bc9bb6f3c74f5
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-05-27 13:44:17 -0400

    icons added to the details

--- src/pages/Plant.js
+++ src/pages/Plant.js
@@ -9,9 +9,17 @@ const Plant = (props) => {
           <img src={props.plant.species.img} alt="plantimage"/>
           <h1>{props.plant.name}</h1>
           <section className= "details">
-          <h4>Frequency: {props.plant.species.frequency}</h4>
-          <h4>Sunlight: {props.plant.species.sunlight}</h4>
-          <h4>Temperature: {props.plant.species.temperature.min} - {props.plant.species.temperature.max}</h4>
+            <h4>
+              <i class="far fa-calendar"></i> 
+              Frequency: {props.plant.species.frequency}
+            </h4>
+            <h4>
+              <i class="fas fa-sun"></i> 
+              Sunlight: {props.plant.species.sunlight}
+            </h4>
+            <h4>
+              <i class="fas fa-temperature-high"></i> Temperature: {props.plant.species.temperature.min} - {props.plant.species.temperature.max}
+            </h4>
           </section>
           <hr/>
           <form><h4>Description: {props.plant.species.description}</h4></form>
